3bbd41456114280c5f0476778749596013e3b2ec
prranavv <pranavvkumar03@gmail.com>
2026-03-14 15:24:25 +0530

    added test for refund

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,2 +1,5 @@
 #[cfg(test)]
 mod test_initialize;
+
+#[cfg(test)]
+mod test_refund;
--- tests/src/test_refund.rs
+++ tests/src/test_refund.rs
@@ -12,8 +12,9 @@ struct InitializeArgs{
     pub seed:u64
 }
 
+
 #[test]
-fn test_initialize_and_take(){
+fn initialize_and_refund(){
     let mut svm = LiteSVM::new();
     let program_keypair = read_keypair_file("../target/deploy/escrow-keypair.json").unwrap();
     let program_id = program_keypair.pubkey();
@@ -44,16 +45,6 @@ fn test_initialize_and_take(){
         .unwrap();
 
     let taker_mint_b_ata = CreateAssociatedTokenAccount::new(&mut svm, &taker, &mint_b)
-        .owner(&taker.pubkey())
-        .send()
-        .unwrap();
-
-    let maker_mint_b_ata = CreateAssociatedTokenAccount::new(&mut svm, &maker, &mint_b)
-        .owner(&maker.pubkey())
-        .send()
-        .unwrap();
-
-    let taker_mint_a_ata = CreateAssociatedTokenAccount::new(&mut svm, &taker, &mint_a)
         .owner(&taker.pubkey())
         .send()
         .unwrap();
@@ -125,64 +116,44 @@ fn test_initialize_and_take(){
     let vault_state=litesvm_token::get_spl_account::<spl_token::state::Account>(&svm, &vault).unwrap();
     assert_eq!(vault_state.amount,1_000_000_000);
 
-    //take instruction
 
-    let take_discriminator_idl=[
-        149,
-        226,
-        52,
-        104,
-        6,
-        142,
-        230,
-        39
-    ];
+    //refund instruction
+    let refund_descriminator_idl=[
+        2,
+        96,
+        183,
+        251,
+        63,
+        208,
+        46,
+        46
+      ];
+      let mut refund_descriminator = [0u8; 8];
+      refund_descriminator.copy_from_slice(&refund_descriminator_idl[..8]);
 
-    let mut take_discriminator = [0u8; 8];
-    take_discriminator.copy_from_slice(&take_discriminator_idl[..8]);
+      let refund_instruction_data = refund_descriminator.to_vec();
+      let refund_instruction=Instruction{
+        program_id,
+        accounts:vec![
+            AccountMeta::new(maker.pubkey(),true),
+            AccountMeta::new_readonly(mint_a, false),
+            AccountMeta::new(maker_mint_a_ata,false),
+            AccountMeta::new(escrow_pda,false),
+            AccountMeta::new(vault,false),
+            AccountMeta::new_readonly(spl_token::id(), false),
+        ],
+        data:refund_instruction_data
+      };
 
-    let take_instruction_data=take_discriminator.to_vec();
+    let refund_tx = Transaction::new_signed_with_payer(&[refund_instruction], Some(&maker.pubkey()), &[&maker], svm.latest_blockhash());
+    svm.send_transaction(refund_tx).unwrap();
 
-    let take_instruction = Instruction{
-    program_id,
-    accounts:vec![
-        AccountMeta::new(taker.pubkey(), true),
-        AccountMeta::new(maker.pubkey(),false),
-        AccountMeta::new_readonly(mint_a, false),
-        AccountMeta::new_readonly(mint_b, false),
-        AccountMeta::new(taker_mint_a_ata, false),
-        AccountMeta::new(taker_mint_b_ata,false),
-        AccountMeta::new(maker_mint_b_ata,false),
-        AccountMeta::new(escrow_pda,false),
-        AccountMeta::new(vault,false),
-        AccountMeta::new_readonly(spl_associated_token_account::id(),false),
-        AccountMeta::new_readonly(spl_token::id(), false),
-        AccountMeta::new_readonly(solana_system_interface::program::ID, false)
-    ],
-    data:take_instruction_data
-    };
-
-    let taker_tx = Transaction::new_signed_with_payer(&[take_instruction], Some(&taker.pubkey()), &[&taker], svm.latest_blockhash());
-    svm.send_transaction(taker_tx).unwrap();
-
-    let escrow_closed =match svm.get_account(&escrow_pda){
+    let maker_mint_a_ata_state =litesvm_token::get_spl_account::<spl_token::state::Account>(&svm, &maker_mint_a_ata).unwrap();
+    assert_eq!(maker_mint_a_ata_state.amount,1_000_000_000,"Maker should have deposited the amount to the vault");
+    
+    let vault_state = match svm.get_account(&vault){
         None=>true,
-        Some(a)=>a.lamports==0 && a.data.is_empty(),
+        Some(account)=>account.lamports==0 && account.data.is_empty()
     };
-    assert!(escrow_closed,"Escrow should be closed");
-
-    let vault_closed = match svm.get_account(&vault){
-        None=>true,
-        Some(a)=>a.lamports==0&& a.data.is_empty(),
-    };
-    assert!(vault_closed,"Vault should be closed");
-
-    let taker_mint_a_ata_state = litesvm_token::get_spl_account::<spl_token::state::Account>(&svm, &taker_mint_a_ata).unwrap();
-    assert_eq!(taker_mint_a_ata_state.amount,1_000_000_000,"Taker should have recieved");
-
-    let taker_mint_b_ata_state=litesvm_token::get_spl_account::<spl_token::state::Account>(&svm, &taker_mint_b_ata).unwrap();
-    assert_eq!(taker_mint_b_ata_state.amount,0,"Taker should all mint b");
-
-    let maker_mint_b_ata_state = litesvm_token::get_spl_account::<spl_token::state::Account>(&svm, &maker_mint_b_ata).unwrap();
-    assert_eq!(maker_mint_b_ata_state.amount,500_000_000,"Maker should have recieved this amount")
+    assert!(vault_state,"Vault is closed")
 }
